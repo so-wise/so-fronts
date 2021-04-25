380eaeb8c4a1168cca4073c9056b0bb1a26cf1a9
Simon Thomas <sdat2@cam.ac.uk>
2021-04-25 13:20:59 +0100

    ext: slimming down make figures file, shortening names

--- docs/api.md
+++ docs/api.md
@@ -5,26 +5,26 @@
     selection:
       docstring_style: reStructuredText  
 
-::: src.plot.cluster_profiles.make_cluster_profiles
+::: src.plot.profiles.make_profiles
     :docstring:
     selection:
       docstring_style: google
       docstring_options:
         replace_admonitions: no
 
-::: src.plot.cluster_profiles.plot_profiles_dataset
+::: src.plot.profiles.plot_profiles
     :docstring:
     selection:
       docstring_style: reStructuredText
       docstring_options:
         replace_admonitions: no
 
-::: src.plot.i_cluster_map_comp.plot_map_imetric_clusters
+::: src.plot.i_map.map_imetric
     :docstring:
     selection:
       docstring_style: reStructuredText
 
-::: src.plot.spec_i_clusters_3d_comp.plot_fig2_mult
+::: src.plot.clust_3d.comp_3d
     :docstring:
     selection:
       docstring_style: reStructuredText
--- src/data_loading/io_name_conventions.py
+++ src/data_loading/io_name_conventions.py
@@ -1,5 +1,46 @@
 """Defaults for naming files."""
 import os
+import xarray as xr
+import src.constants as cst
+import src.time_wrapper as twr
+import src.models.to_pair_i_metric as tpi
+
+
+@twr.timeit
+def return_pair_i_metric(
+    k_clusters: int = cst.K_CLUSTERS, pca: int = cst.D_PCS, save_nc: bool = True
+) -> xr.DataArray:
+    """Return pair i metric.
+
+    Args:
+        k_clusters (int, optional): Number of
+               clusters. Defaults to cst.K_CLUSTERS.
+        pca (int, optional): Number of principal components. Defaults to cst.D_PCS.
+        save_nc (bool, optional): Whether or not to save the resulting dataset.
+            Defaults to True.
+
+    Returns:
+        xr.DataArray: pair i metric.
+    """
+    link_to_netcdf = _return_name(k_clusters, pca) + ".nc"
+    ds = xr.open_dataset(link_to_netcdf)
+    print(ds.__str__())
+    batch_size = 2
+    for i in range(cst.EXAMPLE_TIME_INDEX, cst.EXAMPLE_TIME_INDEX + 2, batch_size):
+        print("running", i)
+        if save_nc:
+            da = tpi.pair_i_metric(
+                ds.isel(time=slice(i, i + batch_size)), threshold=0.05
+            )
+            print("not saving")
+        else:
+            da = (
+                xr.open_dataset(_return_pair_name(k_clusters, pca) + "..nc")
+                .to_array()
+                .isel(time=slice(i, i + batch_size))
+            )
+
+    return da
 
 
 def _return_name(k_clusters: int, pca_components: int) -> str:
