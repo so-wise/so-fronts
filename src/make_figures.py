5c7d86ef1274d851a8ec1fd94327a37cad1b039e
Simon Thomas <sdat2@cam.ac.uk>
2021-04-25 13:20:59 +0100

    ext: slimming down make figures file, shortening names

--- src/make_figures.py
+++ src/make_figures.py
@@ -10,77 +10,11 @@ import src.constants as cst
 import src.plotting_utilities.latex_style as lsty
 import src.plotting_utilities.xarray_panels as xp
 import src.models.train_i_metric as tim
-import src.plot.spec_i_clusters_3d_comp as s3d
-import src.plot.i_cluster_map_comp as icm
-import src.plot.cluster_profiles as cp
-import src.models.to_pair_i_metric as tpi
+import src.plot.clust_3d as c3d
+import src.plot.i_map as imap
+import src.plot.profiles as prof
 import src.data_loading.io_name_conventions as io
 import src.time_wrapper as twr
-
-
-@twr.timeit
-def return_pair_i_metric(
-    K: int = cst.K_CLUSTERS, pca: int = cst.D_PCS, save_nc: bool = True
-) -> xr.DataArray:
-    """Return pair i metric.
-
-    Args:
-        K (int, optional): Number of clusters. Defaults to cst.K_CLUSTERS.
-        pca (int, optional): Number of principal components. Defaults to cst.D_PCS.
-        save_nc (bool, optional): Whether or not to save the resulting dataset.
-            Defaults to True.
-
-    Returns:
-        xr.DataArray: pair i metric.
-    """
-    link_to_netcdf = io._return_name(K, pca) + ".nc"
-    ds = xr.open_dataset(link_to_netcdf)
-    print(ds.__str__())
-    batch_size = 2
-    for i in range(cst.EXAMPLE_TIME_INDEX, cst.EXAMPLE_TIME_INDEX + 2, batch_size):
-        print("running", i)
-        if save_nc:
-            da = tpi.pair_i_metric(
-                ds.isel(time=slice(i, i + batch_size)), threshold=0.05
-            )
-            print("not saving")
-        else:
-            da = (
-                xr.open_dataset(io._return_pair_name(K, pca) + "..nc")
-                .to_array()
-                .isel(time=slice(i, i + batch_size))
-            )
-
-    return da
-
-
-@twr.timeit
-def other() -> None:
-    xp.plot_several_pair_i_metrics(
-        [return_pair_i_metric(K=2).isel(time=0), return_pair_i_metric(K=4).isel(time=0)]
-    )
-    # "../FBSO-Report/images/fig5-new.png"
-    plt.tight_layout()
-    imetric_comp_name = os.path.join(
-        cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_i_metric_comp.png"
-    )
-    plt.savefig(imetric_comp_name)
-    plt.clf()
-
-
-def nother() -> None:
-    ds = xr.open_dataset(cst.DEFAULT_NC)
-    da_temp = ds.PCA_VALUES.isel(time=cst.EXAMPLE_TIME_INDEX).differentiate(cst.Y_COORD)
-    xp.sep_plots(
-        [da_temp.isel(pca=0), da_temp.isel(pca=1), da_temp.isel(pca=2)],
-        ["PC1 y-grad", "PC2 y-grad", "PC3 y-grad"],
-    )
-    pc_y_grad_name = os.path.join(
-        cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_y_grad.png"
-    )
-    # "../FBSO-Report/images/fig6-new.png"
-    plt.savefig(pc_y_grad_name)
-    plt.clf()
 
 
 @twr.timeit
@@ -99,6 +33,7 @@ def make_all_figures_in_sequence() -> None:
         "cst.SEED",
         cst.SEED,
     )
+    lsty.mpl_params()
 
     # FIGURE 1: pc maps.
 
@@ -119,7 +54,7 @@ def make_all_figures_in_sequence() -> None:
     profiles_name = os.path.join(
         cst.DATA_PATH, "RUN_" + cst.RUN_NAME + "_profiles_temp.nc"
     )
-    m, ds = tim.train_on_interpolated_year(
+    pcm, ds = tim.train_on_interpolated_year(
         time_i=cst.EXAMPLE_TIME_INDEX,
         K=cst.K_CLUSTERS,
         maxvar=cst.D_PCS,
@@ -131,13 +66,13 @@ def make_all_figures_in_sequence() -> None:
 
     ds.to_netcdf(path=temp_name)
     ds = xr.open_dataset(temp_name)
-    profile_ds = cp.make_cluster_profiles(ds)
+    profile_ds = prof.make_profiles(ds)
     profile_ds.to_netcdf(path=profiles_name)
     profile_ds = xr.open_dataset(profiles_name)
 
     print(profile_ds)
 
-    cp.plot_profiles_dataset(profile_ds)
+    prof.plot_profiles(profile_ds)
     profiles_plot_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_profiles.png"
     )
@@ -147,8 +82,11 @@ def make_all_figures_in_sequence() -> None:
 
     # FIGURE 2: Plot 3d clusters.
 
-    s3d.plot_fig2_mult(
-        m._classifier.weights_, m._classifier.means_, m._classifier.covariances_, ds
+    c3d.comp_3d(
+        pcm._classifier.weights_,
+        pcm._classifier.means_,
+        pcm._classifier.covariances_,
+        ds,
     )
     s3d_plot_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_s3d_clusters.png"
@@ -176,7 +114,7 @@ def make_all_figures_in_sequence() -> None:
 
     # FIGURE 4: single multi color.
 
-    da = return_pair_i_metric(K=cst.K_CLUSTERS)
+    da = io.return_pair_i_metric(k_clusters=cst.K_CLUSTERS)
     xp.plot_single_i_metric(da.isel(time=0))
     imetric_single_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_i_metric_single.png"
@@ -187,12 +125,12 @@ def make_all_figures_in_sequence() -> None:
 
     # FIGURE 4: Plot clusters and i metrics on maps.
 
-    da = return_pair_i_metric(K=cst.K_CLUSTERS).isel(time=0)
+    da = io.return_pair_i_metric(k_clusters=cst.K_CLUSTERS).isel(time=0)
     da_i = xr.open_dataset(cst.DEFAULT_NC).A_B.isel(rank=0, time=cst.EXAMPLE_TIME_INDEX)
 
     print(da)
 
-    icm.plot_map_imetric_clusters(da_i, da)
+    imap.map_imetric(da_i, da)
     imetric_single_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_map_i_comp.png"
     )
@@ -200,10 +138,13 @@ def make_all_figures_in_sequence() -> None:
     plt.savefig(imetric_single_name)
     plt.clf()
 
-    # FIGURE 5: Plot different K cluster multi colour plots.
+    # FIGURE 5: Plot different k_clusters cluster multi colour plots.
 
     xp.plot_several_pair_i_metrics(
-        [return_pair_i_metric(K=2).isel(time=0), return_pair_i_metric(K=4).isel(time=0)]
+        [
+            io.return_pair_i_metric(k_clusters=2).isel(time=0),
+            io.return_pair_i_metric(k_clusters=4).isel(time=0),
+        ]
     )
     # "../FBSO-Report/images/fig5-new.png"
     plt.tight_layout()
@@ -247,7 +188,7 @@ def make_all_figures_in_sequence() -> None:
             .mean(dim=cst.T_COORD, skipna=True),
             uvel_ds.UVEL.mean(dim=cst.T_COORD, skipna=True),
         ],
-        ["PC1 y-grad", r"$U$ (m s$^{-1}$)", "PC1 y-grad", r"$U$ (m s$^{-1}$)"],
+        ["PC1 y-grad", r"$U$ (pcm s$^{-1}$)", "PC1 y-grad", r"$U$ (pcm s$^{-1}$)"],
     )
     pc_y_grad_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_pc_y_grad.png"
@@ -334,7 +275,7 @@ def make_all_figures_in_sequence() -> None:
             .mean(dim=cst.T_COORD, skipna=True),
             vvel_ds.VVEL.mean(dim=cst.T_COORD, skipna=True),
         ],
-        ["PC1 x-grad", r"$V$ (m s$^{-1}$)", "PC1 x-grad", r"$V$ (m s$^{-1}$)"],
+        ["PC1 x-grad", r"$V$ (pcm s$^{-1}$)", "PC1 x-grad", r"$V$ (pcm s$^{-1}$)"],
     )
     pc_y_grad_name = os.path.join(
         cst.FIGURE_PATH, "RUN_" + cst.RUN_NAME + "_pc_x_grad.png"
