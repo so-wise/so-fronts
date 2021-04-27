d1d890f1c5777540cf715315bafa15d7d76d5ef4
Simon Thomas <sdat2@cam.ac.uk>
2021-04-27 16:39:48 +0100

    ext: adding features to the make figures script, and redoing figure labelling

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 """Run key scripts."""
 import src.make_figures as mf
-
-# import src.move_figures as mov
+import src.move_figures as mov
 
 mf.make_all_figures()
 
-# mov.move()
+mov.move()
 
 # import src.plotting_utilities.ko_plot as ko
 
