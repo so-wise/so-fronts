081408b80f4e6485ab41884d213930c119870cc1
Simon Thomas <sdat2@cam.ac.uk>
2021-09-10 21:54:23 +0100

    ext: changing version number

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.2",
     author="Simon Thomas",
     author_email="sdat2@cam.ac.uk",
     description="Full scripts to generate figures for 'Defining Southern"
-                 + "Ocean fronts using unsupervised classification'",    
+                 + "Ocean fronts using unsupervised classification'",
     url="https://github.com/so-wise/so-fronts",
     packages=find_packages(),
     test_suite="src.tests.test_all.suite",
