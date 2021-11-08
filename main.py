0f09558d88477edf00eba4766c7fd3d2f0767e88
Simon Thomas <sdat2@cam.ac.uk>
2021-11-08 11:41:31 +0000

    ext: removing things

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ mov.move()
 # animate_imetric(video_path="gifs/boundaries-k5.gif", k_clusters=5)
 # animate_imetric(video_path="gifs/boundaries-k4.gif", k_clusters=4)
 # animate_imetric(video_path="gifs/boundaries-k2.gif", k_clusters=2)
+
+"""
+git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch
+figures/RUN_010_pc_x_sobel_comp.pdf
+figures/RUN_010_pc_y_sobel_comp.pdf
+figures/RUN_010_i_metric_comp.pdf
+figures/RUN_010_y_sobel.pdf
+figures/RUN_010_pc_map.pdf
+figures/RUN_010_i_metric_comp.pdf
+figures/RUN_010_i_metric_single.pdf
+figures/RUN_010_map_i_comp.pdf
+figures/RUN_010_x_sobel.pdf
+figures/RUN_010_pc_map.pdf' -- --all
+    """
